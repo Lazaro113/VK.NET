4d90bbad71d2c2d216f317e2e16b2e302e1b47b6
Lazaro113 <lazaroassassino113@gmail.com>
2026-03-26 07:59:55 -0300

    Update

--- utils/scanners/portscanner.py
+++ utils/scanners/portscanner.py
@@ -5,22 +5,22 @@ print("")
 host = input("Digite: ")
 portas = [80, 443, 20, 21, 990, 22, 23, 25, 110, 143, 53]
 
-
 try:
-	for ports in portas:
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		s.settimeout(5.0)
-		code = s.connect_ex((host, ports))
-		if code == 0:
-			
-			print(Fore.GREEN + "[+] A porta", ports, "está aberta")
-			
-			
-		else:
-			print(Fore.RED + "[-] A porta", ports, "está fechada")
-			
-except Exception as e:
-	print("Error", e)
-			
+	with open("testelazaro.txt", "a") as f:
+		f.write("\n" + "=" * 3 + "\n" + host + "\n")
+		for ports in portas:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.settimeout(2.0)
+			code = s.connect_ex((host, ports))
+			if code == 0:
 		
+				msg = f"[+] A porta {ports} está aberta"
+				print(msg)
+				f.write(msg)	
+			else:
+				msg = f"[-] A porta {ports} está fechada"
+				print(msg)
+				f.write(msg)
+except Exception as e:
+	print("Error: ", e)
 
