5016c762d725020ffe33097ac0c1e53a335301e4
Lazaro113 <lazaroassassino113@gmail.com>
2026-03-29 00:01:17 -0300

    Color

--- utils/scanners/portscanner.py
+++ utils/scanners/portscanner.py
@@ -1,10 +1,12 @@
 import socket
-from colorama import Fore
+from colorama import init, Fore
 
 print("")
 host = input("Digite: ")
+print("")
+print("Resultado: utils/scanners/port_scanner.txt")
 portas = [80, 443, 20, 21, 990, 22, 23, 25, 110, 143, 53]
-caminho = "utils/scanners/testelazaro.txt"
+caminho = "utils/scanners/port_scanner.txt"
 
 try:
 	with open(caminho, "a") as f:
@@ -14,12 +16,15 @@ try:
 			s.settimeout(2.0)
 			code = s.connect_ex((host, ports))
 			if code == 0:
-		
-				msg = f"[+] A porta {ports} está aberta"
+				init()
+				print("")
+				msg = Fore.GREEN + f"[+] A porta {ports} está aberta"
 				print(msg)
 				f.write(msg)	
 			else:
-				msg = f"[-] A porta {ports} está fechada"
+				init()
+				print("")
+				msg = Fore.RED + f"[-] A porta {ports} está fechada"
 				print(msg)
 				f.write(msg)
 except Exception as e:
